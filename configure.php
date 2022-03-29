349ac73016b82f5a430773d49d1cb12e2cb2ab4a
kehpaul <40598763+kehpaul@users.noreply.github.com>
2022-03-29 22:46:02 +0100

    Create configure.php

---
+++ configure.php
@@ -0,0 +1,18 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "";
+$dbpassword = "";
+$dbdatabase = "test";
+
+//site name
+
+$config_sitename = "Elliox";
+
+//admin info
+$config_admin = "Kehinde Alare";
+$config_adminemail = "kehinde_alare@elliox.net";
+
+//Location for configuration directory
+$config_basedir = "/employee";
+
+?>
