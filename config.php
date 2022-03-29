0df95f59fecd3269786a58e429a253854d3c9a86
kehpaul <40598763+kehpaul@users.noreply.github.com>
2022-03-29 22:31:11 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "Your username";
+$dbpassword = "Your password";
+$dbdatabase = "test";
+
+//site name
+
+$config_sitename = "Your Sitenane";
+
+//admin info
+$config_admin = "Kehinde Alare";
+$config_adminemail = "kehinde_alare@elliox.net";
+
+//Location for configuration directory
+//change this to your director eg https://elliox.net/test/
+$config_basedir = "your directory";
+
+?>
